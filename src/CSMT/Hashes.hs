91992f1618f28feb2018b0d3163a33634aa889b3
paolino <paolo.veronelli@gmail.com>
2025-11-12 10:32:02 +0000

    feat: add keccak hashes

--- src/CSMT/Hashes.hs
+++ src/CSMT/Hashes.hs
@@ -1,30 +1,25 @@
 {-# LANGUAGE StrictData #-}
 
 module CSMT.Hashes
-    (
+    ( mkHash
+    , addHash
     )
 where
 
--- type Hash = ByteString
--- type SMTMap = Map Key Hash
+import Crypto.Hash (Keccak_256, hash)
+import Data.ByteArray (ByteArray, ByteArrayAccess, convert)
+import Data.ByteString (ByteString)
 
--- mkHash :: ByteString -> Hash
--- mkHash = convert . hash @ByteString @SHA256
+-- | A simple wrapper around Keccak-256 hashes, with a combining function.
+newtype Hash = Hash ByteString
+    deriving
+        (Eq, Ord, Show, Semigroup, Monoid, ByteArrayAccess, ByteArray)
 
--- defaultHash :: Hash
--- defaultHash = mkHash ""
+-- | Create a Keccak-256 hash from a ByteString.
+mkHash :: ByteString -> Hash
+mkHash = convert . hash @ByteString @Keccak_256
 
--- root :: SMTMap -> Maybe Hash
--- root = Map.lookup []
-
--- mkKey :: ByteString -> Key
--- mkKey bs =
---     concatMap byteToKey (ByteString.unpack $ mkHash bs)
---   where
---     byteToKey w = [0 .. 7] <&> fromBool . testBit w
-
--- composeHash :: Hash -> Hash -> Hash
--- composeHash left right = mkHash (left <> right)
-
--- insert :: ByteString -> ByteString -> SMTMap -> SMTMap
--- insert key value = inserting composeHash defaultHash (mkKey key) (mkHash value)
+-- | Combine two hashes by concatenating their ByteString representations
+--   and hashing the result.
+addHash :: Hash -> Hash -> Hash
+addHash (Hash h1) (Hash h2) = mkHash (h1 <> h2)
